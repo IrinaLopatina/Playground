49d6a0be6d2aa8a6bfebf61f3a24ceeff2f6409a
IrinaLopatina <irinalopatina@hotmail.com>
2022-11-14 00:28:42 +0100

    Add test project and some tests

---
+++ SodaMachine/TestSodaMachine/Model/InventoryTest.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SodaMachine.Model.Inventory;
+using System;
+using System.IO;
+
+namespace TestSodaMachine.Model
+{
+    [TestClass]
+    public class InventoryTest
+    {
+        readonly Inventory inventory = new Inventory();
+
+        [TestMethod]
+        public void OrderSoda_WhenUnknownSodaType_GivesMessageAndDoesNotReduceMoney()
+        {
+            //arrange
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            //act
+            var money = inventory.OrderSoda("pepsi", 100);
+
+            //assert
+            Assert.AreEqual(100, money);
+            var output = stringWriter.ToString();
+            Assert.AreEqual("No such soda\r\n", output);
+        }
+
+        [TestMethod]
+        public void OrderSoda_WhenKnownSodaTypeAndEnoughMoney_GivesMessageAndReducesMoneyToZero()
+        {
+            //arrange
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            //act
+            var money = inventory.OrderSoda("coke", 100);
+
+            //assert
+            Assert.AreEqual(0, money);
+            var output = stringWriter.ToString();
+            Assert.AreEqual("Giving coke out\r\nGiving 80 out in change\r\n", output);
+        }
+
+        [TestMethod]
+        public void OrderSoda_WhenKnownSodaTypeAndNotEnoughMoney_GivesMessageAndDoesNotReduceMoney()
+        {
+            //arrange
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            //act
+            var money = inventory.OrderSoda("coke", 15);
+
+            //assert
+            Assert.AreEqual(15, money);
+            var output = stringWriter.ToString();
+            Assert.AreEqual("Need 5 more\r\n", output);
+        }
+
+        [TestMethod]
+        public void OrderSoda_WhenKnownSodaTypeAndEnoughMoneyAndEmptyInventory_GivesMessageAndDoesNotReduceMoney()
+        {
+            //arrange
+            for(int i = 0; i < 3; i++)
+                inventory.OrderSoda("sprite", 20);
+
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            //act
+            var money = inventory.OrderSoda("sprite", 20);
+
+            //assert
+            Assert.AreEqual(20, money);
+            var output = stringWriter.ToString();
+            Assert.AreEqual("No sprite left\r\n", output);
+        }
+    }
+}
---
+++ SodaMachine/TestSodaMachine/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestSodaMachine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestSodaMachine")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8089d0e9-7c04-478e-af3f-68a69d9b1fc7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
