17cd23183bf45faea1d6dd14cc653500aa35a9e9
IrinaLopatina <irinalopatina@hotmail.com>
2022-11-13 19:40:28 +0100

    Extract SodaMachine.

---
+++ SodaMachine/ConsoleApplication1/ConsoleApplication1/Model/SodaMachine.cs
@@ -0,0 +1,65 @@
+﻿using ConsoleApplication1.Model.Inventory;
+using System;
+
+namespace ConsoleApplication1.Model
+{
+    public class SodaMachine
+    {
+        private static int money;
+
+        private readonly IInventory Inventory;
+
+
+        public SodaMachine(IInventory inventory)
+        {
+            Inventory = inventory;
+        }
+
+        /// <summary>
+        /// This is the starter method for the machine
+        /// </summary>
+        public void Start()
+        {
+            while (true)
+            {
+                Console.WriteLine("\n\nAvailable commands:");
+                Console.WriteLine("insert (money) - Money put into money slot");
+                Console.WriteLine("order (coke, sprite, fanta) - Order from machines buttons");
+                Console.WriteLine("sms order (coke, sprite, fanta) - Order sent by sms");
+                Console.WriteLine("recall - gives money back");
+                Console.WriteLine("-------");
+                Console.WriteLine("Inserted money: " + money);
+                Console.WriteLine("-------\n\n");
+
+                var input = Console.ReadLine();
+
+                if (input.StartsWith("insert"))
+                {
+                    //Add to credit
+                    money += int.Parse(input.Split(' ')[1]);
+                    Console.WriteLine("Adding " + int.Parse(input.Split(' ')[1]) + " to credit");
+                }
+                if (input.StartsWith("order"))
+                {
+                    // split string on space
+                    var sodaName = input.Split(' ')[1];
+
+                    money = Inventory.OrderSoda(sodaName, money);
+                }
+                if (input.StartsWith("sms order"))
+                {
+                    var sodaName = input.Split(' ')[2];
+
+                    Inventory.SmsOrderSoda(sodaName);
+                }
+
+                if (input.Equals("recall"))
+                {
+                    //Give money back
+                    Console.WriteLine("Returning " + money + " to customer");
+                    money = 0;
+                }
+            }
+        }
+    }
+}
--- SodaMachine/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ SodaMachine/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,5 +1,5 @@
-﻿using ConsoleApplication1.Model.Inventory;
-using System;
+﻿using ConsoleApplication1.Model;
+using ConsoleApplication1.Model.Inventory;
 
 namespace ConsoleApplication1
 {
@@ -7,62 +7,9 @@ namespace ConsoleApplication1
     {
         private static void Main(string[] args)
         {
-            SodaMachine sodaMachine = new SodaMachine();
-            sodaMachine.Start();
-        }
-    }
-
-    public class SodaMachine
-    {
-        private static int money;
-
-        /// <summary>
-        /// This is the starter method for the machine
-        /// </summary>
-        public void Start()
-        {
             var inventory = new Inventory();
-
-            while (true)
-            {
-                Console.WriteLine("\n\nAvailable commands:");
-                Console.WriteLine("insert (money) - Money put into money slot");
-                Console.WriteLine("order (coke, sprite, fanta) - Order from machines buttons");
-                Console.WriteLine("sms order (coke, sprite, fanta) - Order sent by sms");
-                Console.WriteLine("recall - gives money back");
-                Console.WriteLine("-------");
-                Console.WriteLine("Inserted money: " + money);
-                Console.WriteLine("-------\n\n");
-
-                var input = Console.ReadLine();
-
-                if (input.StartsWith("insert"))
-                {
-                    //Add to credit
-                    money += int.Parse(input.Split(' ')[1]);
-                    Console.WriteLine("Adding " + int.Parse(input.Split(' ')[1]) + " to credit");
-                }
-                if (input.StartsWith("order"))
-                {
-                    // split string on space
-                    var sodaName = input.Split(' ')[1];
-
-                    money = inventory.OrderSoda(sodaName, money);
-                }
-                if (input.StartsWith("sms order"))
-                {
-                    var sodaName = input.Split(' ')[2];
-
-                    inventory.SmsOrderSoda(sodaName);
-                }
-
-                if (input.Equals("recall"))
-                {
-                    //Give money back
-                    Console.WriteLine("Returning " + money + " to customer");
-                    money = 0;
-                }
-            }
+            var sodaMachine = new SodaMachine(inventory);
+            sodaMachine.Start();
         }
     }
 }
