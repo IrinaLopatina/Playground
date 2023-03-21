5f49e0ff458463933d7e4732f070f187560cb942
Irina Lopatina <irinalopatina@hotmail.com>
2023-03-21 15:26:42 +0100

    Added dto classes.

---
+++ WeatherInfoApp/WeatherInfoApp/Dto/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherInfoApp.Dto
+{
+    internal class Location
+    {
+        string Name { get; set; }
+        GeoCoordinate Coordinate { get; set; }
+        string Url { get; set; }
+    }
+}
---
+++ WeatherInfoApp/WeatherInfoApp/Dto/LocationWeatherInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherInfoApp.Dto
+{
+    internal class LocationWeatherInfo
+    {
+        string Name { get; set; }
+        GeoCoordinate Coordinate { get; set; }
+        double Temperature { get; set; }
+        double WindStrength { get; set; }
+        double Humidity { get; set; }
+    }
+}
