8c6e3139db9f01184a3b966a9967d4a0d7d03d85
Irina Lopatina <irinalopatina@hotmail.com>
2024-01-28 12:24:56 +0100

    Introduce InventoryItemTest

---
+++ SodaMachineNew/TestSodaMachineNew/Model/InventoryItemTest.cs
@@ -0,0 +1,29 @@
+﻿using SodaMachineNew.Model.Inventory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSodaMachineNew.Model
+{
+    public class InventoryItemTest
+    {
+        readonly InventoryItem inventoryItem;
+
+        public InventoryItemTest()
+        {
+            inventoryItem = new InventoryItem(new Soda("fanta", 17), 7);
+        }
+
+        [Fact]
+        public void GetInventoryItemByName_WhenExistingItem_ReturnsItem()
+        {
+            //act
+            inventoryItem.ReduceQuantityByNumber(2);
+
+            //assert
+            Assert.Equal(5, inventoryItem.Quantity);
+        }
+    }
+}
